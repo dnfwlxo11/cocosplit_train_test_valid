f872c88d87d0d386f3fc3c79fc309fb86d6db9ad
daechu <dnfwlxo11@naver.com>
2021-01-06 14:07:54 +0900

    add: first commit & code, readme, requirements file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
+
+## ※※※ Base code ※※※
+https://github.com/akarazniewicz/cocosplit / akarazniewicz 
+(coco, train, test split)
+
+- In the existing code, just edit parser, save line and add ratio calculate code
+
+
 # coco-split_train_test_valid
-coco data annotation file split to train, test, valid own ratio
+If you want to separate the [train, test] sets, use the base code,
+and you want to separate the [train, valid, test] sets, use this code.
+
+
+## init enviornment
+pip install -r requirments
+
+
+## How to use?
+$ python coco-split_train_test_valid.py \
+--annotations {annotationsFile, only Json} \
+--train_ratio {own train_ratio} \
+--valid_ratio {own valid_ratio} \
+--test_ratio {own test_ratio} \
+--trainJson_name {save fileName, Has a default} \
+--validJson_name {save fileName, Has a default} \
+--testJson_name {save fileName, Has a default}
+
+
+## Option information
+$ python coco-split_train_test_valid.py -h
---
+++ coco-split_train_test_valid.py
@@ -0,0 +1,66 @@
+import json
+import argparse
+import funcy
+from sklearn.model_selection import train_test_split
+
+parser = argparse.ArgumentParser(description='Splits COCO annotations file into training and test sets.')
+parser.add_argument('annotations', metavar='coco_annotations', type=str,
+                    help='Path to COCO annotations file.')
+parser.add_argument('--train_ratio', type=float, dest='ratio_train', help='set train dataset ratio')
+parser.add_argument('--valid_ratio', type=float,  dest='ratio_valid',help='set valid dataset ratio')
+parser.add_argument('--test_ratio', type=float,  dest='ratio_test',help='set test dataset ratio')
+parser.add_argument('--trainJson_name', type=str, default='train.json', help='Where to store COCO training annotations')
+parser.add_argument('--validJson_name', type=str, default='valild.json', help='Where to store COCO valid annotations')
+parser.add_argument('--testJson_name', type=str, default='test.json', help='Where to store COCO test annotations')
+parser.add_argument('--annotations', dest='annotations', action='store_true',
+                    help='Ignore all images without annotations. Keep only these with at least one annotation')
+
+args = parser.parse_args()
+
+ratio_train = args.ratio_train
+ratio_valid = args.ratio_valid
+ratio_test = args.ratio_test
+
+def save_coco(file, info, licenses, images, annotations, categories):
+    with open(file, 'wt', encoding='UTF-8') as coco:
+        json.dump({ 'info': info, 'licenses': licenses, 'images': images, 
+            'annotations': annotations, 'categories': categories}, coco, indent=2, sort_keys=True)
+
+def filter_annotations(annotations, images):
+    image_ids = funcy.lmap(lambda i: int(i['id']), images)
+    return funcy.lfilter(lambda a: int(a['image_id']) in image_ids, annotations)
+
+def main(args):
+    with open(args.annotations, 'rt', encoding='UTF-8') as annotations:
+        coco = json.load(annotations)
+        info = coco['info']
+        licenses = coco['licenses']
+        images = coco['images']
+        annotations = coco['annotations']
+        categories = coco['categories']
+
+        number_of_images = len(images)
+
+        images_with_annotations = funcy.lmap(lambda a: int(a['image_id']), annotations)
+
+        if args.annotations:
+            images = funcy.lremove(lambda i: i['id'] not in images_with_annotations, images)
+
+        train_before, test = train_test_split(
+            images, test_size=ratio_test)
+
+        ratio_remaining = 1 - ratio_test
+        ratio_valid_adjusted = ratio_valid / ratio_remaining
+
+        train_after, valid = train_test_split(
+            train_before, test_size=ratio_valid_adjusted)
+
+        save_coco(args.trainJson_name, info, licenses, train_after, filter_annotations(annotations, train_after), categories)
+        save_coco(args.testJson_name, info, licenses, test, filter_annotations(annotations, test), categories)
+        save_coco(args.validJson_name, info, licenses, valid, filter_annotations(annotations, valid), categories)
+
+        print("Saved {} entries in {} and {} in {} and {} in {}".format(len(train_after), args.trainJson_name, len(test), args.testJson_name, len(valid), args.validJson_name))
+
+
+if __name__ == "__main__":
+    main(args)
